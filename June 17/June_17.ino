a51ef9de8894316e1d4f63c2fc7eb4c5a0c8c3a4
Ronit Singh <ronit.singh@nyu.edu>
2021-06-16 21:09:45 +0530

    Add files via upload

---
+++ June 17/June_17.ino
@@ -0,0 +1,64 @@
+/*
+  Name: Ronit Singh
+  Date: 17 June 2021
+  HW: Arduino Homework 2
+*/
+
+int light; // for analog sensor
+const int led1 = 13;
+const int led2 = A3;
+const int pushButton = 8;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pushButton, INPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+}
+
+void loop() {
+  light = analogRead(A0);
+  int buttonState = digitalRead(pushButton);
+
+  // if less light detect
+  if (light < 20) {
+
+    // only one of the two LEDs glow
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+
+    // if switch / button pressed, then both LEDs blink
+    if (buttonState == HIGH) {
+      digitalWrite(led1, HIGH);
+      delay(100);
+      digitalWrite(led2, LOW);
+      delay(100);
+
+      digitalWrite(led2, HIGH);
+      delay(100);
+      digitalWrite(led1, LOW);
+      delay(100);
+    }
+  }
+  
+  // if more light detect
+  else if (light > 20) {
+
+    // one of the other LEDs glow
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+
+    // if switch / button pressed, then both LEDs blink
+    if (buttonState == HIGH) {
+      digitalWrite(led1, HIGH);
+      delay(100);
+      digitalWrite(led2, LOW);
+      delay(100);
+
+      digitalWrite(led2, HIGH);
+      delay(100);
+      digitalWrite(led1, LOW);
+      delay(100);
+    }
+  }
+}
